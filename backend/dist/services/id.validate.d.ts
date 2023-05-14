88f7737b77ae42f3fbad44efefa93badc6ce441d
boburf <changeitgo3@gmail.com>
2023-05-14 22:49:39 +0500

    feat: webpack

--- backend/dist/services/id.validate.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function validId(id: string): void;
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -20,5 +22,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
